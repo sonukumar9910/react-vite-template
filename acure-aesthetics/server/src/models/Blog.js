7c1c92f5bd21a4454bbbb42618d6c35ac2f1b78f
sonukumar9910 <91356276+sonukumar9910@users.noreply.github.com>
2025-06-12 12:46:58 +0530

    Add acure-aesthetics/server/src/models/Blog.js 1749712617504

--- acure-aesthetics/server/src/models/Blog.js
+++ acure-aesthetics/server/src/models/Blog.js
@@ -1,13 +1,58 @@
-class Blog {
-  static async find() {
-    return global.inMemoryDB.blogs || [];
-  }
+const mongoose = require('mongoose');
 
-  static async findById(id) {
-    return (global.inMemoryDB.blogs || []).find(blog => blog.id === id);
+const commentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  comment: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
+});
 
-  static async create(data) {
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String
+  },
+  likes: {
+    type: Number,
+    default: 0
+  },
+  comments: [commentSchema],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// If MongoDB is not connected, use in-memory database
+const Blog = mongoose.models.Blog || mongoose.model('Blog', blogSchema);
+
+if (!mongoose.connection.readyState) {
+  Blog.find = async () => global.inMemoryDB.blogs || [];
+  Blog.findById = async (id) => (global.inMemoryDB.blogs || []).find(blog => blog.id === id);
+  Blog.create = async (data) => {
     const blog = {
       id: Date.now().toString(),
       ...data,
@@ -16,34 +61,26 @@ class Blog {
       createdAt: new Date(),
       updatedAt: new Date()
     };
-
-    if (!global.inMemoryDB.blogs) {
-      global.inMemoryDB.blogs = [];
-    }
-
+    if (!global.inMemoryDB.blogs) global.inMemoryDB.blogs = [];
     global.inMemoryDB.blogs.push(blog);
     return blog;
-  }
-
-  static async updateById(id, data) {
+  };
+  Blog.updateById = async (id, data) => {
     const index = (global.inMemoryDB.blogs || []).findIndex(blog => blog.id === id);
     if (index === -1) return null;
-    
     global.inMemoryDB.blogs[index] = {
       ...global.inMemoryDB.blogs[index],
       ...data,
       updatedAt: new Date()
     };
     return global.inMemoryDB.blogs[index];
-  }
-
-  static async deleteById(id) {
+  };
+  Blog.deleteById = async (id) => {
     const index = (global.inMemoryDB.blogs || []).findIndex(blog => blog.id === id);
     if (index === -1) return false;
-    
     global.inMemoryDB.blogs.splice(index, 1);
     return true;
-  }
+  };
 }
 
 module.exports = Blog;
