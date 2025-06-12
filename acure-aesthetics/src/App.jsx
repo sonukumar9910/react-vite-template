f00dc8eb3e1c3b1500c02511cc38798ac3bd5290
sonukumar9910 <91356276+sonukumar9910@users.noreply.github.com>
2025-06-12 11:40:09 +0530

    Add acure-aesthetics/src/App.jsx 1749708608305

---
+++ acure-aesthetics/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
+import Blog from './pages/Blog'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white flex flex-col">
+      <Navbar />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
